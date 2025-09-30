9379dee5dda7dfccd062f7c4a8111d8457ba5590
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-09-30 05:56:45 +0530

    feat: Use improved config plugin for manifest fix

--- withAndroidComponentFactory.js
+++ withAndroidComponentFactory.js
@@ -1,27 +1,26 @@
-// /mobile-app/withAndroidComponentFactory.js
-
+// /withAppComponentFactoryFix.js
 const { withAndroidManifest } = require('@expo/config-plugins');
 
-module.exports = function withAndroidComponentFactory(config) {
-  return withAndroidManifest(config, async (config) => {
-    // Get the main <application> tag
-    const mainApplication = config.modResults.manifest.application[0];
+module.exports = function withAppComponentFactoryFix(config) {
+  return withAndroidManifest(config, (config) => {
+    const manifest = config.modResults;
 
-    // Ensure the attributes object ($) exists
-    if (!mainApplication.$) {
-      mainApplication.$ = {};
-    }
-    
-    // Add the 'tools:replace' attribute to the <application> tag
-    mainApplication.$['tools:replace'] = 'android:appComponentFactory';
-    
-    // Also add the tools namespace to the root <manifest> tag
-    if (!config.modResults.manifest.$) {
-      config.modResults.manifest.$ = {};
-    }
-    if (!config.modResults.manifest.$['xmlns:tools']) {
-        config.modResults.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
-    }
+    // Ensure the tools namespace exists on <manifest>
+    if (!manifest.manifest.$) manifest.manifest.$ = {};
+    manifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+
+    // Find the <application> element
+    const app = manifest.manifest.application?.[0];
+    if (!app) return config;
+
+    if (!app.$) app.$ = {};
+
+    // Prefer AndroidX factory and tell merger to replace
+    app.$['android:appComponentFactory'] = 'androidx.core.app.CoreComponentFactory';
+    app.$['tools:replace'] = [
+      app.$['tools:replace'],
+      'android:appComponentFactory',
+    ].filter(Boolean).join(',');
 
     return config;
   });
